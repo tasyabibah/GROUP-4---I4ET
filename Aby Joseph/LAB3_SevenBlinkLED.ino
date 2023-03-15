e6148ccfcd8d17c27060238fcd96db9c120d7527
Abyjoe100 <127529692+Abyjoe100@users.noreply.github.com>
2023-03-15 15:31:15 +0100

    Add files via upload

---
+++ Aby Joseph/LAB3_SevenBlinkLED.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(D1,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(D1,HIGH);
+delay(1000); // For a delay of 1 second
+}
